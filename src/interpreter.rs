517a4cb0e05235c0fb65561d00d02fe5efd64e9c
Samuel Nösslböck <samuel.noesslboeck@gmail.com>
2022-11-12 00:37:27 +0100

    Delayed interpreter

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -1,20 +1,23 @@
 use crate::SyArm;
 use stepper_lib::gcode::{GCode, GCodeFunc, NumEntries, Letter, LetterEntries, Interpreter};
 
-pub fn init_interpreter(syarm : SyArm) -> (SyArm, Interpreter) {
-    let g0 : GCodeFunc = |intpr : &Interpreter, code : &GCode| { 
-        // syarm.debug_pins();
-        None
-    };
+// pub fn init_interpreter<'a>(syarm : &SyArm) -> Interpreter<'static, SyArm> {
+//     let g0 : GCodeFunc<SyArm> = |intpr : &Interpreter<'a, SyArm>, code : &GCode, arm : &SyArm| { 
+//         // syarm.debug_pins();
+//         None
+//     };
 
-    let funcs = LetterEntries::from([
-        (Letter::General, NumEntries::from([
-            (0, g0)
-        ])),
-        (Letter::Miscellaneous, NumEntries::from([
+//     let funcs = LetterEntries::from([
+//         (Letter::General, NumEntries::from([
+//             (0, g0)
+//         ])),
+//         (Letter::Miscellaneous, NumEntries::from([
 
-        ]))
-    ]);
+//         ]))
+//     ]);
 
-    return (syarm, Interpreter::new(funcs));
-}
+//     return Interpreter {
+//         funcs,
+//         mach: syarm
+//     };
+// }
