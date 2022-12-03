63bc3f7484502196b95c3abc4ce8a5377b3d325c
SamuelNoesslboeck <samuel.noesslboeck@gmail.com>
2022-12-03 18:57:23 +0000

    Sync

--- bin/test_ex_move.rs
+++ bin/test_ex_move.rs
@@ -4,6 +4,7 @@ use std::{time::Duration, thread::sleep, f32::consts::PI};
 
 fn main() {
     let mut syarm = SyArm::load_json("res/syarm_const.json");
+    syarm.init_meas();
 
     println!("Measuring ... ");
 
@@ -12,6 +13,6 @@ fn main() {
     syarm.update_sim();
 
     println!("Exact positioning");
-    syarm.drive_a3_abs(0.0);
+    // syarm.drive_a3_abs(0.0);
     println!("Done!");
 }
