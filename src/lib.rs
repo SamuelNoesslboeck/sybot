9a6603c0ce408e8c5ac861625a9ab2f3da4cfe1a
Samuel Nösslböck <samuel.noesslboeck@gmail.com>
2022-12-13 22:19:09 +0100

    New async test

--- src/lib.rs
+++ src/lib.rs
@@ -577,17 +577,11 @@ impl SyArm
             self.ctrl_a3.measure(-2.0*PI, self.cons.omega_3,  self.cons.meas_a3, accuracy);
         }
 
-        // pub async fn measure_async(&mut self, accuracy : u64) {
-        //     let mut self_mutex = Arc::new(Mutex::new(self)));
-
-        //     thread::spawn(move || {
-        //         let mut self_mutex_clone = Arc::clone(&self_mutex);
-        //         let mut self_m = self_mutex_clone.lock().unwrap();
-        //         self.ctrl_a1.cylinder.measure(self_m.cons.l_c1a + self.cons.l_c1b, self.cons.c1_v, false, self.cons.meas_a1, accuracy)
-        //     });
-        //     thread::spawn(|| self.ctrl_a2.cylinder.measure(self.cons.l_c2a + self.cons.l_c2b, self.cons.c2_v, false, self.cons.meas_a2, accuracy));
-        //     thread::spawn(move || self.ctrl_a3.measure(2.0*PI, self.cons.omega_3, false, self.cons.meas_a3, accuracy));
-        // }
+        pub fn measure_async(&mut self, accuracy : u64) {
+            self.ctrl_a1.cylinder.measure_async(-(self.cons.l_c1a + self.cons.l_c1b), self.cons.c1_v, self.cons.meas_a1, accuracy);
+            self.ctrl_a2.cylinder.measure_async(-(self.cons.l_c2a + self.cons.l_c2b), self.cons.c2_v, self.cons.meas_a2, accuracy);
+            self.ctrl_a3.measure_async(-2.0*PI, self.cons.omega_3,  self.cons.meas_a3, accuracy);
+        }
     //
 
     // Async control
