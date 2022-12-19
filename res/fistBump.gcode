f05212ec66746b01e9756e97ecbc75faca09d5ae
Samuel Nösslböck <114945809+SamuelNoesslboeck@users.noreply.github.com>
2022-12-19 19:19:52 +0100

    Interpreter

--- res/fistBump.gcode
+++ res/fistBump.gcode
@@ -2,12 +2,12 @@
 ; 18/12/2022
 G28; Measure
 
-G8 X-400 Y0 Z300; Position behind
-G4 X1           ; Wait 1 sec
-G8 X-520 Y0 Z300; Position front
-G4 X1           ; Wait 1 sec
-G0 X-400 Y0 Z300; Position behind
+; Fistbump program
+G8 X-400 Y0 Z300    ; Position behind
+G4 X1               ; Wait 1 sec
+G8 X-520 Y0 Z300    ; Position front
+G8 X-400 Y0 Z300    ; Position behind
 
-G8 X0 Y380 Z400 ; Correct home position
+G8 X0 Y380 Z400     ; Correct home position
 
 G28; Back to home
--- src/interpreter.rs
+++ src/interpreter.rs
@@ -4,21 +4,21 @@ use stepper_lib::gcode::*;
 use crate::{SyArm, SyArmResult};
 
 // General Functions
-    /// G0 X<Position> Y<Position> Z<Position>
+    /// G0 X{Position} Y{Position} Z{Position} D{Angle} \
     /// Rapid positioning
     pub fn g0(arm : &mut SyArm, _code : &GCode, args : &Args) -> SyArmResult<Value> {
         let angles = arm.get_with_fixed_dec_s(
             get_arg_letter(args, 'X'), 
             get_arg_letter(args, 'Y'), 
             get_arg_letter(args, 'Z'), 
-                None
+            get_arg_letter(args, 'D')
         )?; 
         arm.drive_to_angles(arm.gammas_for_phis(&angles));
         arm.update_sim();
         Ok(serde_json::json!(vec![angles.0, angles.1, angles.2, angles.3]))
     }
 
-    /// G4 X<Position> Y<Position> Z<Position>
+    /// G4 X{Seconds} P{Milliseconds}
     /// Dwell (sleeping)
     pub fn g4(_ : &mut SyArm, _code : &GCode, args : &Args) -> SyArmResult<Value> {
         let seconds = 
@@ -28,76 +28,78 @@ use crate::{SyArm, SyArmResult};
         Ok(serde_json::json!(seconds))
     }
 
-    /// G8 X<Position> Y<Position> Z<Position>
+    /// G8 X{Position} Y{Position} Z{Position} D{Angle} \
     /// Rapid positioning async
     pub fn g8(arm : &mut SyArm, _code : &GCode, args : &Args) -> SyArmResult<Value> {
         let angles = arm.get_with_fixed_dec_s(
             get_arg_letter(args, 'X'), 
             get_arg_letter(args, 'Y'), 
             get_arg_letter(args, 'Z'), 
-                None
+            get_arg_letter(args, 'D')
         )?; 
         arm.drive_to_angles_async(arm.gammas_for_phis(&angles));
         arm.await_inactive();
         arm.update_sim();
         Ok(serde_json::json!(vec![angles.0, angles.1, angles.2, angles.3]))
-    }
+    }   
 
-    /// G10 C<Angle>
-    /// Moves the base with a relative angle
-    pub fn g10(arm : &mut SyArm, _code : &GCode, args : &Args) -> SyArmResult<Value> {
-        arm.drive_base_rel(args[0].value);
-        Ok(Value::Null)
-    }
+    // Incorrect time measurement
+        /// G10 C<Angle>
+        /// Moves the base with a relative angle
+        pub fn g10(arm : &mut SyArm, _code : &GCode, args : &Args) -> SyArmResult<Value> {
+            arm.drive_base_rel(args[0].value);
+            Ok(Value::Null)
+        }
 
-    /// G11 A<Angle>
-    /// Moves the first arm segment with a relative angle
-    pub fn g11(arm : &mut SyArm, _code : &GCode, args : &Args) -> SyArmResult<Value> {
-        arm.drive_a1_rel(args[0].value);
-        Ok(Value::Null)
-    }
+        /// G11 A<Angle>
+        /// Moves the first arm segment with a relative angle
+        pub fn g11(arm : &mut SyArm, _code : &GCode, args : &Args) -> SyArmResult<Value> {
+            arm.drive_a1_rel(args[0].value);
+            Ok(Value::Null)
+        }
 
-    /// G12 A<Angle>
-    /// Moves the second arm segment with a relative angle
-    pub fn g12(arm : &mut SyArm, _code : &GCode, args : &Args) -> SyArmResult<Value> {
-        arm.drive_a2_rel(args[0].value);
-        Ok(Value::Null)
-    }
+        /// G12 A<Angle>
+        /// Moves the second arm segment with a relative angle
+        pub fn g12(arm : &mut SyArm, _code : &GCode, args : &Args) -> SyArmResult<Value> {
+            arm.drive_a2_rel(args[0].value);
+            Ok(Value::Null)
+        }
 
-    /// G13 A<Angle>
-    /// Moves the third arm segment with a relative angle
-    pub fn g13(arm : &mut SyArm, _code : &GCode, args : &Args) -> SyArmResult<Value> {
-        arm.drive_a3_rel(args[0].value);
-        Ok(Value::Null)
-    }
+        /// G13 A<Angle>
+        /// Moves the third arm segment with a relative angle
+        pub fn g13(arm : &mut SyArm, _code : &GCode, args : &Args) -> SyArmResult<Value> {
+            arm.drive_a3_rel(args[0].value);
+            Ok(Value::Null)
+        }
 
-    /// G20 C<Angle>
-    /// Moves the base with an absolute angle
-    pub fn g20(arm : &mut SyArm, _code : &GCode, args : &Args) -> SyArmResult<Value> {
-        arm.drive_base_rel(args[0].value);
-        Ok(Value::Null)
-    }
+        /// G20 C<Angle>
+        /// Moves the base with an absolute angle
+        pub fn g20(arm : &mut SyArm, _code : &GCode, args : &Args) -> SyArmResult<Value> {
+            arm.drive_base_rel(args[0].value);
+            Ok(Value::Null)
+        }
 
-    /// G21 A<Angle>
-    /// Moves the first arm segment with an absolute angle
-    pub fn g21(arm : &mut SyArm, _code : &GCode, args : &Args) -> SyArmResult<Value> {
-        arm.drive_a1_rel(args[0].value);
-        Ok(Value::Null)
-    }
+        /// G21 A<Angle>
+        /// Moves the first arm segment with an absolute angle
+        pub fn g21(arm : &mut SyArm, _code : &GCode, args : &Args) -> SyArmResult<Value> {
+            arm.drive_a1_rel(args[0].value);
+            Ok(Value::Null)
+        }
 
-    /// G22 A<Angle>
-    /// Moves the second arm segment with an absolute angle
-    pub fn g22(arm : &mut SyArm, _code : &GCode, args : &Args) -> SyArmResult<Value> {
-        arm.drive_a2_rel(args[0].value);
-        Ok(Value::Null)
-    }
+        /// G22 A<Angle>
+        /// Moves the second arm segment with an absolute angle
+        pub fn g22(arm : &mut SyArm, _code : &GCode, args : &Args) -> SyArmResult<Value> {
+            arm.drive_a2_rel(args[0].value);
+            Ok(Value::Null)
+        }
 
-    /// G23 A<Angle>
-    /// Moves the third arm segment with an absolute angle
-    pub fn g23(arm : &mut SyArm, _code : &GCode, args : &Args) -> SyArmResult<Value> {
-        arm.drive_a3_rel(args[0].value);
-        Ok(Value::Null)
-    }
+        /// G23 A<Angle>
+        /// Moves the third arm segment with an absolute angle
+        pub fn g23(arm : &mut SyArm, _code : &GCode, args : &Args) -> SyArmResult<Value> {
+            arm.drive_a3_rel(args[0].value);
+            Ok(Value::Null)
+        }
+    // 
 
     /// G28 \
     /// Return to home position
