eab9f0e98a20768390e8b0245e4f76a10537d232
Samuel Nösslböck <114945809+SamuelNoesslboeck@users.noreply.github.com>
2023-04-20 16:15:38 +0200

    fixed tests

--- src/lib.rs
+++ src/lib.rs
@@ -20,9 +20,11 @@ sybot_lib = { version = \"0.8.0\", features = [ \"rasp\" ] }
 ```
 
 ```rust
-use sybot_lib::{SyArm, Robot, JsonConfig};
+use sybot_lib::Robot;
+use sybot_lib::conf::JsonConf;
 use sybot_lib::intpr::Interpreter;
 use sybot_lib::intpr::gcode::init_intpr;
+use sybot_lib::robot::SyArm;
 
 fn main() -> std::io::Result<()> {
     // Load the standard-partlibs in order to use motor names as data
